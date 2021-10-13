d594fa2149278d7bf80b9478ef334cfc5dc51b1d
iamnizall <89332706+iamnizall@users.noreply.github.com>
2021-10-14 05:13:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="asset/style/style.css">
+  <link rel="shortcut icon" href="asset/img/logoN.png">
+  <title>Pemrograman Web</title>
+</head>
+<body>
+
+<header>
+
+<nav>
+    <ul>
+      <li class="logo">UNIVERSITAS TEKNOLOGI YOGYAKARTA</li>
+      <div class="nav">
+      
+      <li><a class="active" href="#home">Home</a></li>
+      <li><a href="#news">News</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+    </ul>
+
+    </div>
+</nav>
+
+<div class="text">
+  <h2>THAT HORIZON MIGHT BE CLOSER THAN YOU THINK.</h2>
+  <h3>We'll help you get there by managing our money and budgets better every day</h3>
+
+</div>
+
+
+
+  <div class="box">
+    
+    <button type="submit">SIGN-IN</button>
+  </div>
+
+</header>
+
+  
+</body>
+</html>
